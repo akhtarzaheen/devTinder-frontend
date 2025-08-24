6567b6324d3ab2f36f31b796f7222beefbe2aa38
Zaheen Akhtar <Personal@Roopyas-MacBook-Air.local>
2025-08-24 23:28:31 +0530

    integrated login api

--- devTinder-web/README.md
+++ devTinder-web/README.md
@@ -10,4 +10,7 @@
 - Created BrowserRouter and Routes > Route = /Body > Route Children  
 - Created an outlet for the body
 - Created a Footer.jsx component file
-
+- created login page
+- installed axios 
+- installed CORS in backend => added middleware with configuration => origin, methods, credentials
+- From frontend passed {withCredentials: true} to axios with every front api integration
--- devTinder-web/src/Login.jsx
+++ devTinder-web/src/Login.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { useState } from "react";
+import icon from "../public/developer-icon.jpg";
+import axios from "axios";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [emailId, setEmailId] = useState("zaheen@gmail.com");
+  const [password, setPassword] = useState("Zaheen@2021");
 
-export default Login
+  const handleLogin = async () => {
+    try{
+    let response = await axios.post("http://localhost:4000/login",{
+      emailId,
+      password
+    },
+  { withCredentials: true });
+    }catch(error){
+      console.log(error);
+    }
+  };
+
+  return (
+    <div>
+      <div className="card card-side bg-base-300 shadow-sm flex justify-center align-center ml-[25%] mx-20 my-10 rounded-2xl w-1/2">
+        <figure>
+          <img src={icon} alt="Developer Icon" className="w-96" />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title flex justify-center">Login</h2>
+          <fieldset className="fieldset">
+            <legend className="fieldset-legend">Email ID</legend>
+            <input type="email" value={emailId} onChange={(e) => setEmailId(e.target.value)} className="input" placeholder="Type here" />
+          </fieldset>
+          <fieldset className="fieldset">
+            <legend className="fieldset-legend">Password</legend>
+            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="input" placeholder="Type here" />
+          </fieldset>
+          <div className="card-actions justify-center">
+            <button className="btn btn-primary" onClick={handleLogin}>Login</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
