0967d9e1246aa9d3820174bcb8946b8459a44919
Zaheen Akhtar <Personal@Roopyas-MacBook-Air.local>
2025-09-03 21:42:06 +0530

    feat: logout feature

--- devTinder-web/src/components/NavBar.jsx
+++ devTinder-web/src/components/NavBar.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
-import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux'
+import { Link, useNavigate } from 'react-router-dom';
+import { BASE_URL } from '../utils/constants';
+import { removeUser } from '../utils/userSlice';
+import axios from 'axios';
 
 const NavBar = () => {
   let user = useSelector((state) => state.user);
-  console.log('user=> ', user?.data?.photoURL);
+  let dispatch = useDispatch();
+  let navigate = useNavigate();
+  const handleLogout = async () => {
+    try{
+      await axios.post(BASE_URL+"/logout",{},{withCredentials: true});
+      dispatch(removeUser());
+      navigate("/login");
+    }catch(err){
+      console.log(err);
+    }
+  }
   return (
     <div className="navbar bg-base-300 shadow-sm px-5">
   <div className="flex-1">
@@ -31,7 +44,7 @@ const NavBar = () => {
           </Link>
         </li>
         <li><a>Settings</a></li>
-        <li><a>Logout</a></li>
+        <li><a onClick={handleLogout}>Logout</a></li>
       </ul>
     </div>
     )}
